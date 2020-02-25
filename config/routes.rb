4724e2ee078a74410be8ca9681930d2a061febe0
James Grunewald <james.grunewald.10@ucl.ac.uk>
2020-02-25 10:40:57 +0000

    routes finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[index show new create edit update] do
+    resources :bookings, only: %i[index new create]
+  end
+  resources :bookings, only: %i[destroy]
 end
