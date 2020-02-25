0e7c7374f35ca8814a4d7f88b3d8bd322e3b2882
Jack Bodley-Scott <jackso.bodleyscott@gmail.com>
2020-02-25 11:03:34 +0000

    added the is cook attribute to the creation of a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   has_many :cooks, :through => :bookings
   has_many :renters, :class_name => "Booking", :foreign_key => "cook_id"
   has_many :rented_cooks, :through => :renters, :source => :user
-
   has_one_attached :avatar
   has_many_attached :photos
+
+  validates :iscook, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
