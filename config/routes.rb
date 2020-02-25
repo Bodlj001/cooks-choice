b70fac8b79ef9fa224dd23eb3bbbaf11a0ab897a
James Grunewald <james.grunewald.10@ucl.ac.uk>
2020-02-25 10:59:21 +0000

    routes cleaned up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users do
-    resources :bookings, only: [:new, :create]
-  end
-  resources :bookings, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show new create edit update] do
     resources :bookings, only: %i[index new create]
