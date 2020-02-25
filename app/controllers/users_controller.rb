050c620514450984ed548586d081682d1d685b75
Karl Loomes <kloomes@hotmail.com>
2020-02-25 13:12:25 +0000

    Show action and basic view added, bookings model changed to add foreign key

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @bookings = Booking.where(cook: @user)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  belongs_to :cook, :class_name => 'User'
+  belongs_to :cook, class_name: 'User', foreign_key: :cook_id
 end
